5eb207edf052a2e72dd99f52b99e354c2eb3dfd8
Priyacom <priya880866@gmail.com>
2024-05-14 12:04:43 +0530

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*======== typing animation ===========*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Graphic Designer","Web Developer","Contant Creator"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
